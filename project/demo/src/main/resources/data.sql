4f3e3af393292d43c40b740cd1904211029dff98
Jason Ho <alanho123@gmail.com>
2019-02-18 16:02:22 +0800

    update to version 0.0.2

--- project/demo/src/main/resources/data.sql
+++ project/demo/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 insert into users (id, account, username, age, balance) values (1, 'account1', '張三', 20, 100.0);
 insert into users(id, account, username, age, balance) values (2, 'account2', '李四', 28, 180.0);
 insert into users(id, account, username, age, balance) values (3, 'account3', '王五', 32, 280.0);
+insert into users(id, account, username, age, balance) values (4, 'account4', '趙六', 37, 390.0);
